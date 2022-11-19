0677fca418a261a78e7a1e7bf4454536b1ab9715
Carol Wanjohi <wanjohi.carol54@gmail.com>
2022-11-19 13:33:00 +0300

    feat: [SearchResults] use bootstrap and change base URL based on environment

--- src/app/deezer-api/deezer-api.service.ts
+++ src/app/deezer-api/deezer-api.service.ts
@@ -2,19 +2,21 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { SearchDataResponse } from './interfaces';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeezerApiService {
+  readonly baseUrl: string = environment.apiUrl;
   constructor(
     private readonly http: HttpClient,
   ) {}
-
-  // TODO: FETCH BASE URL FROM environment file
   search$(value: string): Observable<SearchDataResponse> {
-    const params: HttpParams | undefined = value ? new HttpParams().set('q', `artist:"${value}"`) : undefined;
-    return this.http.request<SearchDataResponse>('GET', 'http://cors-anywhere.herokuapp.com/https://api.deezer.com/search', {
+    const params: HttpParams | undefined = value ? new HttpParams()
+      .set('q', `artist:"${value}"`)
+      .set('order', 'RATING_DESC') : undefined;
+    return this.http.request<SearchDataResponse>('GET',   `${this.baseUrl}/search`, {
         params
       }
     );
--- src/app/home/components/search-results/search-results.module.ts
+++ src/app/home/components/search-results/search-results.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchResultsComponent } from './search-results.component';
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
   imports: [
     CommonModule,
+    MatCardModule
   ],
   declarations: [
     SearchResultsComponent
