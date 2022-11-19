b2d3fbe8a5aa9e27bc2d84d80d534782df12c448
Carol Wanjohi <wanjohi.carol54@gmail.com>
2022-11-19 14:13:12 +0300

    test: [Search] test artist's search and remove redundant test

--- src/app/deezer-api/deezer-api.service.spy.ts
+++ src/app/deezer-api/deezer-api.service.spy.ts
@@ -1,5 +1,5 @@
 import { DeezerApiService } from './deezer-api.service';
 
-export function deezerApiSpy(): jasmine.SpyObj<DeezerApiService> {
+export function deezerApiServiceSpy(): jasmine.SpyObj<DeezerApiService> {
   return jasmine.createSpyObj('DeezerApiService', ['search$']);
 }
